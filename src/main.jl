03d64befc239bc0b4c3a401845a8400d19f57361
Niklas Lollo <nicklollo2@gmail.com>
2017-11-28 20:30:16 -0800

    RCall

--- src/main.jl
+++ src/main.jl
@@ -5,28 +5,64 @@ include("../../mimi-page.jl/src/getpagefunction.jl") # Needs modification specif
 m = getpage()
 run(m)
 
-temp_df = DataFrame(
-temp = m[:ClimateTemperature,:rt_g_globaltemperature])
+## Years
+year = [2009, 2010, 2020, 2030, 2040, 2050, 2075, 2100, 2150, 2200]
 
-adapt_df = DataFrame(m[:TotalAdaptationCosts,
-:act_adaptationcosts_total])
-preventative_costs = DataFrame(m[:TotalAbatementCosts, :tct_totalcosts])
+## Population
+pop = m[:Population,:pop_population]
+pop_compare = readpagedata(m,"../mimi-page.jl/test/validationdata/pop_population.csv")
 
-slrdamages = DataFrame(m[:SLRDamages, :rcons_per_cap_SLRRemainGDP])
-market_df = DataFrame(m[:MarketDamages, :rcons_per_cap_MarketRemainGDP])
-nonmarket_df = DataFrame(m[:NonMarketDamages, :rcons_per_cap_NonMarketRemainGDP])
+## GDP
+gdp = m[:GDP,:gdp]
+gdp_compare = readpagedata(m,"../mimi-page.jl/test/validationdata/gdp.csv")
 
-writetable("temp.csv", temp_df)
-writetable("adapt.csv", adapt_df)
-writetable("prevent.csv", preventative_costs)
-writetable("slrdamages.csv", slrdamages)
-writetable("marketdamages.csv", market_df)
-writetable("nonmarketdamages.csv", nonmarket_df)
+## Emissions
+emissions = m[:co2emissions,:e_globalCO2emissions]
+emissions_compare = readpagedata(m,"../mimi-page.jl/test/validationdata/e_globalCO2emissions.csv")
+emissions_df = DataFrame(year = year, emissions = emissions, emissions_compare = emissions_compare)
 
+## Temperature
+temp = m[:ClimateTemperature,:rt_g_globaltemperature]
+temp_compare = readpagedata(m,"../mimi-page.jl/test/validationdata/rt_g_globaltemperature.csv")
+temp_df = DataFrame(year = year, temp = temp, temp_compare = temp_compare)
 
-# This file + RCall -> ggplot
-# Review test_mainmodel for which variables and to read in validation data
-# Pop, GDP, emissions, Temp, damages
+## Damages
+# From Discontinuity
+discdamages = m[:Discontinuity,:rcons_per_cap_DiscRemainConsumption]
+discdamages_compare = readpagedata(m,"../mimi-page.jl/test/validationdata/rcons_per_cap_DiscRemainConsumption.csv")
+
+# From SLR
+slrdamages = m[:SLRDamages,:rcons_per_cap_SLRRemainConsumption]
+slrdamages_compare = readpagedata(m, "../mimi-page.jl/test/validationdata/rcons_per_cap_SLRRemainConsumption.csv")
+
+# Market damages
+mdamages = m[:MarketDamages,:rcons_per_cap_MarketRemainConsumption]
+mdamages_compare = readpagedata(m,"../mimi-page.jl/test/validationdata/rcons_per_cap_MarketRemainConsumption.csv")
+
+# Non-Market Damages
+nmdamages = m[:NonMarketDamages,:rcons_per_cap_NonMarketRemainConsumption]
+nmdamages_compare = readpagedata(m,"../mimi-page.jl/test/validationdata/rcons_per_cap_NonMarketRemainConsumption.csv")
+
+# Sum Damages
+total_damages =
+
+using RCall
+
+R"library(tidyverse)"
+# Population Plot
+
+# GDP Plot
+
+# Temperature Plot
+R"temp_df = $temp_df %>% gather(temp, temp_compare, key = model, value = temperature)"
+R"temp_plot = ggplot(temp_df, aes(x = year, y = temperature)) + geom_line(aes(color = model))"
+
+# Emissions ggplot
+R"emissions_df = $emissions_df %>% gather(emissions, emissions_compare, key = model, value = emissions)"
+R"ggplot(emissions_df, aes(x = year, y = emissions)) + geom_line(aes(color = model))"
+
+# Damages Plot
+
 # Sum over regions
 
 # Probabilistic Model title
