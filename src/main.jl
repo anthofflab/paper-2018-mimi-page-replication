250bb69b3c365bfe30e340b981e27a6eaf9a6e13
Niklas Lollo <nicklollo2@gmail.com>
2017-11-30 13:25:26 -0800

    improve plots

--- src/main.jl
+++ src/main.jl
@@ -11,22 +11,22 @@ year = [2009, 2010, 2020, 2030, 2040, 2050, 2075, 2100, 2150, 2200]
 ## Population
 pop = vec(sum(m[:Population,:pop_population], 2))
 pop_compare = vec(sum(readpagedata(m,"../mimi-page.jl/test/validationdata/pop_population.csv"),2))
-pop_df = DataFrame(year = year, pop = pop, pop_compare = pop_compare)
+pop_df = DataFrame(year = year, MimiPAGE = pop, PAGE09 = pop_compare)
 
 ## GDP
 gdp = vec(sum(m[:GDP,:gdp], 2))
 gdp_compare = vec(sum(readpagedata(m,"../mimi-page.jl/test/validationdata/gdp.csv"), 2))
-gdp_df = DataFrame(year = year, gdp = gdp, gdp_compare = gdp_compare)
+gdp_df = DataFrame(year = year, MimiPAGE = gdp, PAGE09 = gdp_compare)
 
 ## Emissions
 emissions = m[:co2emissions,:e_globalCO2emissions]
 emissions_compare = readpagedata(m,"../mimi-page.jl/test/validationdata/e_globalCO2emissions.csv")
-emissions_df = DataFrame(year = year, emissions = emissions, emissions_compare = emissions_compare)
+emissions_df = DataFrame(year = year, MimiPAGE = emissions, PAGE09 = emissions_compare)
 
 ## Temperature
 temp = m[:ClimateTemperature,:rt_g_globaltemperature]
 temp_compare = readpagedata(m,"../mimi-page.jl/test/validationdata/rt_g_globaltemperature.csv")
-temp_df = DataFrame(year = year, temp = temp, temp_compare = temp_compare)
+temp_df = DataFrame(year = year, MimiPAGE = temp, PAGE09 = temp_compare)
 
 ## Damages
 # From Discontinuity
@@ -45,35 +45,76 @@ mdamages_compare = vec(sum(readpagedata(m,"../mimi-page.jl/test/validationdata/r
 nmdamages = vec(sum(m[:NonMarketDamages,:rcons_per_cap_NonMarketRemainConsumption],2))
 nmdamages_compare = vec(sum(readpagedata(m,"../mimi-page.jl/test/validationdata/rcons_per_cap_NonMarketRemainConsumption.csv"),2))
 
+# Total damages
+MimiPAGE_total = discdamages + slrdamages + mdamages + nmdamages
+PAGE09_total = discdamages_compare + slrdamages_compare + mdamages_compare + nmdamages_compare
+
 damages_df = DataFrame(year = year,
-    discdamages = discdamages, discdamages_compare = discdamages_compare,
-    slrdamages = slrdamages, slrdamages_compare = slrdamages_compare,
-    mdamages = mdamages, mdamages_compare = mdamages_compare,
-    nmdamages = nmdamages, nmdamages_compare = nmdamages_compare,
-    total_damages = discdamages + slrdamages + mdamages + nmdamages,
-    total_damages_compare = discdamages_compare + slrdamages_compare + mdamages_compare + nmdamages_compare)
+                        MimiPAGE_disc = discdamages, PAGE09_disc = discdamages_compare,
+                        MimiPAGE_slr = slrdamages, PAGE09_slr = slrdamages_compare,
+                        MimiPAGE_mkt = mdamages, PAGE09_mkt = mdamages_compare,
+                        MimiPAGE_nonmkt = nmdamages, PAGE09_nonmkt = nmdamages_compare,
+                        MimiPAGE = MimiPAGE_total, PAGE09 = PAGE09_total)
 
 using RCall
 
 R"library(tidyverse)"
 # Population Plot
-R"pop_df = $pop_df %>% gather(pop, pop_compare, key = model, value = population)"
-R"pop_plot = ggplot(pop_df, aes(x = year, y = population)) + geom_line(aes(color = model))"
+R"pop_df = $pop_df %>%
+    gather(MimiPAGE, PAGE09, key = model, value = population)"
+R"pop_plot = ggplot(pop_df, aes(x = year, y = population)) +
+    geom_line(aes(color = model))+
+    geom_point(aes(shape = model))+
+    scale_shape_manual(values=c(6, 17))+
+    labs(title = 'Comparing Mimi-PAGE and PAGE09',
+        subtitle = 'Population',
+        y = 'Population (million persons)',
+        x = 'Year')"
 
 # GDP Plot
-R"gdp_df = $gdp_df %>% gather(gdp, gdp_compare, key = model, value = gdp)"
-R"gdp_plot = ggplot(gdp_df, aes(x = year, y = gdp)) + geom_line(aes(color = model))"
+R"gdp_df = $gdp_df %>%
+    gather(MimiPAGE, PAGE09, key = model, value = gdp)"
+R"gdp_plot = ggplot(gdp_df, aes(x = year, y = gdp)) +
+    geom_line(aes(color = model))+
+    geom_point(aes(shape = model))+
+    scale_shape_manual(values=c(6, 17))+
+    labs(title = 'Comparing Mimi-PAGE and PAGE09',
+        subtitle = 'GDP',
+        y = 'GDP (million USD)',
+        x = 'Year')"
 
 # Temperature Plot
-R"temp_df = $temp_df %>% gather(temp, temp_compare, key = model, value = temperature)"
-R"temp_plot = ggplot(temp_df, aes(x = year, y = temperature)) + geom_line(aes(color = model))"
+R"temp_df = $temp_df %>%
+    gather(MimiPAGE, PAGE09, key = model, value = temperature)"
+R"temp_plot = ggplot(temp_df, aes(x = year, y = temperature)) +
+    geom_line(aes(color = model)) +
+    geom_point(aes(shape = model))+
+    scale_shape_manual(values=c(6, 17))+
+    labs(title = 'Comparing Mimi-PAGE and PAGE09',
+        subtitle = 'Temperature',
+        y = 'Temperature (deg C)',
+        x = 'Year')"
 
 # Emissions ggplot
-R"emissions_df = $emissions_df %>% gather(emissions, emissions_compare, key = model, value = emissions)"
-R"ggplot(emissions_df, aes(x = year, y = emissions)) + geom_line(aes(color = model))"
+R"emissions_df = $emissions_df %>%
+    gather(MimiPAGE, PAGE09, key = model, value = emissions)"
+R"ggplot(emissions_df, aes(x = year, y = emissions)) +
+    geom_line(aes(color = model))+
+    geom_point(aes(shape = model))+
+    scale_shape_manual(values=c(6, 17))+
+    labs(title = 'Comparing Mimi-PAGE and PAGE09',
+        subtitle = 'Emissions',
+        y = 'Emissions (tons CO2)',
+        x = 'Year')"
 
 # Damages Plot
-R"tot_damages_df = $damages_df %>% gather(total_damages, total_damages_compare, key = model, value = damages)"
-R"tot_damages_plot = ggplot(tot_damages_df, aes(x = year, y = damages)) + geom_line(aes(color = model))"
-
-# Probabilistic Model title
+R"tot_damages_df = $damages_df %>%
+    gather(MimiPAGE, PAGE09, key = model, value = damages)"
+R"tot_damages_plot = ggplot(tot_damages_df, aes(x = year, y = damages)) +
+    geom_line(aes(color = model))+
+    geom_point(aes(shape = model))+
+    scale_shape_manual(values=c(6, 17))+
+    labs(title = 'Comparing Mimi-PAGE and PAGE09',
+        subtitle = 'Total Damages',
+        y = 'Total Damages (million USD)',
+        x = 'Year')"
